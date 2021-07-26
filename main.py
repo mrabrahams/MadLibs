58a1e459a7ad0cdffa236329a02b5f1b50fb787e
mrabrahams <62486253+mrabrahams@users.noreply.github.com>
2021-07-26 12:24:04 -0500

    Learning string concat, variables, and receiving input in Python through use of madlibs game.

---
+++ main.py
@@ -0,0 +1,14 @@
+# # learning string concatenation by creating a madlib
+# print("some string " + variable)
+# print("some string {}".format(variable))
+# print(f"some string {variable}") <-- preferred method
+
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}"
+
+
